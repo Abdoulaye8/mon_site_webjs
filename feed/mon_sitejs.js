15b219fd1f223a0882d05b9b41302391c72f56c1
abdoulaye <abdoulaye.soumare@le-campus-numerique.fr>
2025-04-30 16:49:33 +0200

    à terminer

--- feed/mon_sitejs.js
+++ feed/mon_sitejs.js
@@ -35,3 +35,8 @@ getJokes(5);
 function maFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
+ document.getElementById('jokesformulaire').addEventListener('
+document.    
+)
+
+ 

543c2617414dcb4b11454bfd6853949a22cb3465
abdoulaye <abdoulaye.soumare@le-campus-numerique.fr>
2025-08-24 15:52:35 +0200

    correction and comlement main

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const postsContainer = document.getElementById("posts");
 const refreshBtn = document.getElementById("refreshBtn");
 const postForm = document.getElementById("postForm");
 const postContent = document.getElementById("postContent");
-
+// fonction et param
 function createPost(content) {
   const post = document.createElement("div");
   post.className = "post";
   post.textContent = content;
   postsContainer.appendChild(post);
 }
-
+// even
 refreshBtn.addEventListener("click", () => {
   fetch("https://v2.jokeapi.dev/joke/Any?lang=fr&type=single")
     .then((res) => res.json())
@@ -26,3 +26,12 @@ postForm.addEventListener("submit", (e) => {
     postContent.value = "";
   }
 });
+
+// selection du dom par classe, balise
+const postsByClass = document.getElementsByClassName("post");
+const firstHeader = document.getElementsByTagName("header")[0];
+
+// Exempl d'utilisation en affichant le nombre de posts dans le title du header
+if (firstHeader) {
+  firstHeader.title = `Nombre de posts: ${postsByClass.length}`;
+}
